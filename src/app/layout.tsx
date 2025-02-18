d6116891df2f97b50123052915a9de72d966c304
mohan.sharma <mohan.sharma@xcelore.com>
2025-02-19 01:05:39 +0530

    iframe added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,28 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <iframe
+        id="myIframe"
+        allow="microphone"
+        src="http://qa-app.exei.ai/sdk/6790fa2f0bcaccd55ebf5e04?mic=true&channelType=IFRAME&API_KEY=a801104387ac4f4d82ae1b584863f600"
+        style={{
+          position: 'fixed',
+          bottom: '85px',
+          right: '20px',
+          width: '400px',
+          height: '70%',
+          border: 'none',
+          zIndex: 9999,
+        }}
+        title="Exei SDK"
+      >
+        {/* <p>Your browser does not support iframes.</p> */}
+      </iframe>
+      <Script
+        src="https://exei-bkt-important-object.s3.ap-south-1.amazonaws.com/script-qa-minified.js"
+        strategy="afterInteractive" // Loads the script after the page becomes interactive
+      />
+
         {children}
       </body>
     </html>
